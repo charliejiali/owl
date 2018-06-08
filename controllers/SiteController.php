9ab841056bd1e1ee30806fabf4b5379b2d7a3ed9
zhangjiali <charliejiali@hotmail.com>
2018-06-08 18:23:20 +0800

    添加修改密码功能

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -34,10 +34,10 @@ class SiteController extends UserAuthController{
             $msg=$login;
         }
 
-        return json_encode([
+        return json_encode(array(
             "r"=>$r,
             "msg"=>$msg,
-        ]);
+        ));
     }
     public function actionItensynLogin(){
         $get=Yii::$app->request->get();
@@ -53,18 +53,38 @@ class SiteController extends UserAuthController{
             $r=0;
             $msg=$login;
         }
-        return json_encode([
+        return json_encode(array(
             "r"=>$r,
             "msg"=>$msg
-        ]);
+        ));
     }
     public function actionLogout(){
-        $r=0;
-        $msg="";
         $class_user=new User;
         $class_user->logout();
     }
     public function actionChangePassword(){
+        return $this->render('pwd_change');
+    }
+    public function actionUpdatePassword(){
+        $post=Yii::$app->request->post();
 
+        $old_password=$post["old"];
+        $new_password=$post["new"];
+        $confirm_password=$post["confirm"];
+
+        $class_user=new User;
+        $user_id=$this->user_id;
+        $result=$class_user->update_password($user_id,$old_password,$new_password,$confirm_password);
+        if($result===true){
+            $r=1;
+            $msg="密码修改成功";
+        }else{
+            $r=0;
+            $msg=$result;
+        }
+        return json_encode(array(
+            "r"=>$r,
+            "msg"=>$msg
+        ));
     }
 }
