d0281f01e35ae491e14d4f655e0a4f99ad7f118f
zhangjiali <charliejiali@hotmail.com>
2018-06-13 18:41:35 +0800

    jquery->vue

--- views/site/index.php
+++ views/site/index.php
@@ -6,16 +6,16 @@ $this->registerJsFile('/js/pages/login.js');
     <div class="logo"></div>
     <div class="login-box">
         <h2 class="title">用户登录</h2>
-        <div class="form-login pure-form">
+        <div id="login" class="form-login pure-form">
             <!-- form 拷贝的原有标签 -->
-                <input id="email" type="email" placeholder="用户名/邮箱/手机号" class="input-label label-email">
-                <input id="password" type="password" placeholder="密码" class="input-label label-passwd">
-                <label for="remember" class="pure-checkbox">
-                    <input id="remember" type="checkbox"> 自动登录
-                </label>
-                <p class="text-center">
-                    <button type="button" class="pure-btn btn-large btn-submit" id="confirm">登 录</button>
-                </p>
+            <input v-model="email" type="email" placeholder="用户名/邮箱/手机号" class="input-label label-email" >
+            <input v-model="password" type="password" placeholder="密码" class="input-label label-passwd" value="">
+            <label for="auto" class="pure-checkbox">
+                <input id="auto" type="checkbox" v-model="auto_login"> 自动登录
+            </label>
+            <p class="text-center">
+                <button type="button" class="pure-btn btn-large btn-submit" v-on:click="login">登 录</button>
+            </p>
         </div>
     </div>
 <?= $this->render('../module/footer') ?>
