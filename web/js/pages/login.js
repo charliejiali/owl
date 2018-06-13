d4b267ff4d53d06dcdb6f1bfd81738e6ff8ef5eb
zhangjiali <charliejiali@hotmail.com>
2018-06-13 18:42:18 +0800

    jquery->vue(unfinished)

--- web/js/pages/login.js
+++ web/js/pages/login.js
@@ -1,55 +1,86 @@
-$('document').ready(function(){
-    // 回车键
-    $('input').on('keydown',function(e){
-        if(e.keyCode==13){
-            login();
-        }
-    });
-    // 用户登录
-    $('#confirm').on('click', function () {
-        login();
-    });
-
-    function login(){
-        var input = {};
-        input.email = $('#email').val();
-        input.password = $('#password').val();
-        input.auto_login=$('#remember').is(':checked')?1:0;
-
-        $.get('/site/login', input, function (json) {
-            if (json.r == 1) {
-                window.location.href = '/site/mode';
-            }else if(json.r==-1){
-                //itensyn_login();
-            } else {
-                __BDP.alertBox("提示",json.msg);
-            }
-        }, 'json');
-
-        function itensyn_login(){
-            $.ajax({
-                url:'http://www.itensyn.com/api/check_user.php',
-                dataType:'jsonp',
-                data:{
-                    email:$('#email').val(),
-                    password:$('#password').val(),
-                    system_id:2
-                },
-                success:function(json){
-                    console.log(json);return false;
-                    if(json.r==0){
-                        __BDP.alertBox("提示",json.msg);
-                    }else{
-                        $.get('/site/itensyn-login',{user_id:json.user_id,token:json.token},function(json){
-                            if (json.r == 1) {
-                                window.location.href = '/site/mode';
-                            } else {
-                                __BDP.alertBox("提示",json.msg);
-                            }
-                        },'json');
-                    }
-                }
-            });
-        }
+window.onload=function(){
+    var data={
+        email:'',
+        password:'',
+        auto_login:false
     }
-});
+    new Vue({
+        el: '#login',
+        data:data,
+        methods:{
+            login:function(){
+                axios
+                    .get('/site/login',{params:data})
+                    .then(function(json){
+                        console.log(json)
+                        var data=json.data;
+                        if (data.r == 1) {
+                            window.location.href = '/site/mode';
+                        }else if(data.r==-1){
+                            //itensyn_login();
+                        } else {
+                            __BDP.alertBox("提示",data.msg);
+                        }
+                    });
+            }
+        }
+    })
+}
+
+
+
+//$('document').ready(function(){
+//    // 回车键
+//    $('input').on('keydown',function(e){
+//        if(e.keyCode==13){
+//            login();
+//        }
+//    });
+//    // 用户登录
+//    $('#confirm').on('click', function () {
+//        login();
+//    });
+//
+//    function login(){
+//        var input = {};
+//        input.email = $('#email').val();
+//        input.password = $('#password').val();
+//        input.auto_login=$('#remember').is(':checked')?1:0;
+//
+//        $.get('/site/login', input, function (json) {
+//            if (json.r == 1) {
+//                window.location.href = '/site/mode';
+//            }else if(json.r==-1){
+//                //itensyn_login();
+//            } else {
+//                __BDP.alertBox("提示",json.msg);
+//            }
+//        }, 'json');
+//
+//        function itensyn_login(){
+//            $.ajax({
+//                url:'http://www.itensyn.com/api/check_user.php',
+//                dataType:'jsonp',
+//                data:{
+//                    email:$('#email').val(),
+//                    password:$('#password').val(),
+//                    system_id:2
+//                },
+//                success:function(json){
+//                    console.log(json);return false;
+//                    if(json.r==0){
+//                        __BDP.alertBox("提示",json.msg);
+//                    }else{
+//                        $.get('/site/itensyn-login',{user_id:json.user_id,token:json.token},function(json){
+//                            if (json.r == 1) {
+//                                window.location.href = '/site/mode';
+//                            } else {
+//                                __BDP.alertBox("提示",json.msg);
+//                            }
+//                        },'json');
+//                    }
+//                }
+//            });
+//        }
+//    }
+//});
